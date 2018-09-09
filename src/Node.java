7d38374e86d5a6245ef32b4557a1d90d262b6da6
Jeffrey Hernandez <jeffredh@usc.edu>
2018-09-09 14:11:07 -0700

    update build.xml to handle more tasks, added contstructors for the classes in src. Working on parser to handle file parsing

---
+++ src/Node.java
@@ -0,0 +1,21 @@
+public class Node
+{
+    private String name;
+    /**
+     * 
+     * @param name
+     */
+    public Node(String name)
+    {
+        this.name = name;
+    }
+    /**
+     * 
+     * @return
+     */
+    public String getName()
+    {
+        return this.name;
+    }
+    
+};
